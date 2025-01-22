c7237b850053115ce1ba16c23655a452b6efa610
thiago <thiagouk85@icloud.com>
2025-01-22 14:17:11 +0100

    Calculator first screen

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,27 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Recuperar os compoonentes EditText
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.edit_weight)
+        val edtHeight = findViewById<TextInputEditText>(R.id.edit_height)
+
+        val btnCalculate = findViewById<Button>(R.id.btn_calcular)
+
+       btnCalculate.setOnClickListener{
+           val weight = edtWeight.text
+           val height = edtHeight.text
+
+           println("thiago"+weight)
+       }
+
     }
 }
